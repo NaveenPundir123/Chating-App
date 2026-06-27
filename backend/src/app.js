6cf2ab0594cf0b5df51816a932f4e1a7d775dc61
NaveenPundir123 <naveensinghpundir123@gmail.com>
2026-06-27 23:03:58 +0530

    Dockers setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,23 @@
-const { clerkMiddleware } = require("@clerk/express");
 const express = require("express");
 const cors = require("cors");
+const { clerkMiddleware } = require("@clerk/express");
+const fs = require("fs");
+const path = require("path");
+
 const FRONTEND_URL = process.env.FRONTEND_URL;
+const publicDir = path.join(process.cwd(), "public");
 
 const app = express();
 app.use(express.json());
 app.use(clerkMiddleware());
+
+if (fs.existsSync(publicDir)) {
+  app.use(express.static(publicDir));
+  app.get("/{*any}", (req, res, next) => {
+    res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+  });
+}
+
 app.use(cors({ origin: FRONTEND_URL, credentials: true }));
 
 module.exports = app;
