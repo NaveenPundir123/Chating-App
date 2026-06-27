aa8c0cc9ed4da9bd59db6ea96df917bba5cd4aea
NaveenPundir123 <naveensinghpundir123@gmail.com>
2026-06-28 00:10:25 +0530

    add cron job to send health check req every 14 min

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, ".env") });
 const app = require("./src/app");
 const connectDB = require("./src/db/db");
+const { default: job } = require("./src/lib/cron");
 
 if (!process.env.MONGO_URI) {
   console.error(
@@ -13,4 +14,5 @@ connectDB();
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  if (process.env.NODE_ENV === "production") job.start();
 });
