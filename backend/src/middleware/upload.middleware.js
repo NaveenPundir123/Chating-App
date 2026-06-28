0da0d5bb872e9296b74d117b8322a76b98d116ff
NaveenPundir123 <naveensinghpundir123@gmail.com>
2026-06-28 21:26:51 +0530

    fixing

--- backend/src/middleware/upload.middleware.js
+++ backend/src/middleware/upload.middleware.js
@@ -2,7 +2,7 @@ const multer = require("multer");
 
 const MAX_FILE_SIZE = 25 * 1024 * 1024; // 25mb
 
-export const upload = multer({
+const upload = multer({
   storage: multer.memoryStorage(),
   limits: { fileSize: MAX_FILE_SIZE },
   fileFilter: (req, file, cb) => {
@@ -17,3 +17,4 @@ export const upload = multer({
     cb(null, true);
   },
 });
+module.exports = upload;
